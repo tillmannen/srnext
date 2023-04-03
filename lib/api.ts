f56dfdeeb0b349135cdad8af4acd8bde2613a43a
fredrik.tillman <fredrik.tillman@gmail.com>
2023-04-03 10:55:32 +0200

    version 3

--- lib/api.ts
+++ lib/api.ts
@@ -1,41 +1,55 @@
-import axios from 'axios';
+import axios from "axios";
 
-const baseUrl = 'https://api.sr.se/api/v2';
+const baseUrl = "https://api.sr.se/api/v2";
 
 export interface Program {
-    id: number;
-    name: string;
-    description: string;
-    programimage: string;
+  id: number;
+  name: string;
+  description: string;
+  programimage: string;
 }
 
-const findBestMatchingCategory = (urlCategory: string, categories: Category[]): Category | undefined => {
-    return categories.find((category) => {
-        const categoryName = category.name.toLowerCase();
-        return categoryName.startsWith(urlCategory) || categoryName.includes(`a${urlCategory}`);
-    });
+const replaceSwedishCharacters = (input: string): string => {
+  return input.replace(/å/g, "a").replace(/ä/g, "a").replace(/ö/g, "o");
 };
 
-export const fetchProgramsByCategory = async (urlCategory: string): Promise<Program[]> => {
-    const categories = await fetchCategories();
-    const bestMatchingCategory = findBestMatchingCategory(urlCategory, categories);
-
-    if (!bestMatchingCategory) {
-        return [];
-    }
-
-    const response = await axios.get(`${baseUrl}/programs/index?filter=programcategoryid:${bestMatchingCategory.id}&format=json&pagination=false`);
-    return response.data.programs;
+const findBestMatchingCategory = (
+  urlCategory: string,
+  categories: Category[]
+): Category | undefined => {
+  return categories.find((category) => {
+    const categoryName = replaceSwedishCharacters(category.name.toLowerCase());
+    return categoryName.startsWith(urlCategory);
+  });
 };
 
+export const fetchProgramsByCategory = async (
+  urlCategory: string
+): Promise<Program[]> => {
+  const categories = await fetchCategories();
+  const bestMatchingCategory = findBestMatchingCategory(
+    urlCategory,
+    categories
+  );
+
+  if (!bestMatchingCategory) {
+    return [];
+  }
+
+  const response = await axios.get(
+    `${baseUrl}/programs/index?programcategoryid=${bestMatchingCategory.id}&format=json&pagination=false`
+  );
+  return response.data.programs;
+};
 
 export interface Category {
-    id: number;
-    name: string;
+  id: number;
+  name: string;
 }
 
 export const fetchCategories = async (): Promise<Category[]> => {
-    const response = await axios.get(`${baseUrl}/programcategories?format=json&pagination=false`);
-    return response.data.programcategories;
+  const response = await axios.get(
+    `${baseUrl}/programcategories?format=json&pagination=false`
+  );
+  return response.data.programcategories;
 };
-
