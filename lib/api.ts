1af2485275842f307d5d97489c4f57313f78447f
fredrik.tillman <fredrik.tillman@gmail.com>
2023-04-03 10:21:12 +0200

    version 2

--- lib/api.ts
+++ lib/api.ts
@@ -3,13 +3,39 @@ import axios from 'axios';
 const baseUrl = 'https://api.sr.se/api/v2';
 
 export interface Program {
-  id: number;
-  name: string;
-  description: string;
-  programimage: string;
+    id: number;
+    name: string;
+    description: string;
+    programimage: string;
 }
 
-export const fetchProgramsByCategory = async (category: string): Promise<Program[]> => {
-  const response = await axios.get(`${baseUrl}/programs/index?filter=programcategory.name:${encodeURIComponent(category)}&format=json&pagination=false`);
-  return response.data.programs;
+const findBestMatchingCategory = (urlCategory: string, categories: Category[]): Category | undefined => {
+    return categories.find((category) => {
+        const categoryName = category.name.toLowerCase();
+        return categoryName.startsWith(urlCategory) || categoryName.includes(`a${urlCategory}`);
+    });
 };
+
+export const fetchProgramsByCategory = async (urlCategory: string): Promise<Program[]> => {
+    const categories = await fetchCategories();
+    const bestMatchingCategory = findBestMatchingCategory(urlCategory, categories);
+
+    if (!bestMatchingCategory) {
+        return [];
+    }
+
+    const response = await axios.get(`${baseUrl}/programs/index?filter=programcategoryid:${bestMatchingCategory.id}&format=json&pagination=false`);
+    return response.data.programs;
+};
+
+
+export interface Category {
+    id: number;
+    name: string;
+}
+
+export const fetchCategories = async (): Promise<Category[]> => {
+    const response = await axios.get(`${baseUrl}/programcategories?format=json&pagination=false`);
+    return response.data.programcategories;
+};
+
--- pages/kategori/[category].tsx
+++ pages/kategori/[category].tsx
@@ -8,6 +8,7 @@ const CategoryPage: React.FC = () => {
   const { category } = router.query;
 
   const [programs, setPrograms] = useState<Program[]>([]);
+  const sortedPrograms = [...programs].sort((a, b) => a.name.localeCompare(b.name, 'sv'));
 
   useEffect(() => {
     if (category && typeof category === 'string') {
@@ -17,7 +18,9 @@ const CategoryPage: React.FC = () => {
     }
   }, [category]);
 
-  const groupedPrograms = programs.reduce((groups: Record<string, Program[]>, program) => {
+  
+  const groupedPrograms = sortedPrograms.reduce((groups: Record<string, Program[]>, program) => {
+  //const groupedPrograms = programs.reduce((groups: Record<string, Program[]>, program) => {
     const firstLetter = program.name.charAt(0).toUpperCase();
     if (!groups[firstLetter]) {
       groups[firstLetter] = [];
@@ -26,10 +29,25 @@ const CategoryPage: React.FC = () => {
     return groups;
   }, {});
 
+  const letterLinks = Object.keys(groupedPrograms).map((letter) => (
+    <a key={letter} href={`#${letter}`} className="button is-light">
+      {letter}
+    </a>
+  ));
+
+
   return (
     <div className="container">
+      <div className="level is-mobile">
+        <div className="level-left">
+          <div className="level-item">
+            <div className="buttons has-addons">{letterLinks}</div>
+          </div>
+        </div>
+      </div>
       {Object.entries(groupedPrograms).map(([letter, group]) => (
-        <div key={letter}>
+        <div key={letter} id={letter}>
+          <div key={letter}>
           <h2 className="title is-2">{letter}</h2>
           <div className="columns is-multiline">
             {group.map((program) => (
@@ -52,6 +70,7 @@ const CategoryPage: React.FC = () => {
               </div>
             ))}
           </div>
+        </div>
         </div>
       ))}
     </div>
